ad9b9ef278c297516e61f048e4810fe7a20fad98
Blair Frandeen <blairfrandeen@gmail.com>
2024-03-27 16:49:47 -0700

    auto formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
+#include <Eigen/Dense>
 #include <cmath>
 #include <iostream>
-#include <Eigen/Dense>
 
 #include "robotlib.h"
 
@@ -11,20 +11,14 @@ using Eigen::Vector3d;
 
 using namespace std;
 
-
-
-int main()
-{
-
-    Vector3d v(4,3,30);
-    Vector3d u(5,5,45);
+int main() {
+    Vector3d v(4, 3, 30);
+    Vector3d u(5, 5, 45);
     cout << "v = " << endl << v << endl;
     cout << "u = " << endl << u << endl;
     cout << "T(v) =" << endl << utoi(v) << endl;
     cout << "T_inv(v) =" << endl << itransform(utoi(v)) << endl;
     cout << "T(u) =" << endl << utoi(u) << endl;
-    cout << "T(u) * T(v) =" << endl << tmult(u,v) << endl;
-
+    cout << "T(u) * T(v) =" << endl << tmult(u, v) << endl;
     return 0;
 }
-
