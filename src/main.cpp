03e3bbeda3c770136361c75c480ac947204866cc
Blair Frandeen <blairfrandeen@gmail.com>
2024-04-03 15:58:41 -0700

    fix issues with kin

--- src/main.cpp
+++ src/main.cpp
@@ -18,19 +18,14 @@ Matrix3d link_transform(const double link_len, double theta_rad) {
     // distrance transform for link length
     Matrix3d dist_transform = Matrix3d::Identity();
     dist_transform(0, 2) = link_len;
-    /* cout << "dist transform" << endl << dist_transform << endl; */
 
     // rotation transform for link angle
-    Matrix3d rot_transform = Matrix3d::Zero();
+    Matrix3d rot_transform = Matrix3d::Identity();
     rot_transform.topLeftCorner(2, 2) =
         Rotation2D<double>(theta_rad).toRotationMatrix();
-    rot_transform(2, 2) = 1;
-    /* cout << "rot transform" << endl << rot_transform << endl; */
 
     // transform describing end of link 1 relative to the base
-    /* Matrix3d link_transform = tmult(dist_transform, rot_transform); */
-    Matrix3d link_transform = dist_transform * rot_transform;
-    /* cout << "link transform" << endl << link_transform << endl; */
+    Matrix3d link_transform = tmult(rot_transform, dist_transform);
 
     return link_transform;
 }
@@ -41,15 +36,20 @@ Matrix3d link_transform(const double link_len, double theta_rad) {
 
    Returns wrist frame relative to base frame
 */
-Matrix3d kin(Vector3d joint_angles_deg) {
-    // both links have length 0.5
-    const double L1 = 0.5, L2 = 0.5;
+Matrix3d kin(Vector3d joint_angles_deg, Vector3d link_lengths_m) {
+    Matrix3d link1_rel_b =
+        link_transform(link_lengths_m[0], radians(joint_angles_deg[0]));
+    Matrix3d link2_rel_1 =
+        link_transform(link_lengths_m[1], radians(joint_angles_deg[1]));
+    Matrix3d link3_rel_2 =
+        link_transform(link_lengths_m[2], radians(joint_angles_deg[2]));
 
-    Matrix3d link1_rel_b = link_transform(L1, radians(joint_angles_deg[0]));
-    Matrix3d link2_rel_1 = link_transform(L2, radians(joint_angles_deg[1]));
-    Matrix3d link3_rel_2 = link_transform(0, radians(joint_angles_deg[2]));
-
-    Matrix3d wrelb = tmult(link3_rel_2, tmult(link2_rel_1, link1_rel_b));
+    /* Matrix3d wrelb = tmult(link3_rel_2, tmult(link2_rel_1, link1_rel_b)); */
+    /* Matrix3d wrelb = link3_rel_2 * link2_rel_1 * link1_rel_b; */
+    /* Matrix3d wrelb = link2_rel_1 * link1_rel_b; */
+    /* Matrix3d link2_rel_b = link1_rel_b * link2_rel_1; */
+    Matrix3d link2_rel_b = tmult(link1_rel_b, link2_rel_1);
+    Matrix3d wrelb = tmult(link2_rel_b, link3_rel_2);
 
     return wrelb;
 }
@@ -66,8 +66,10 @@ int main() {
     cout << "T(u) * T(v) =" << endl << tmult(u, v) << endl;
     */
 
-    Vector3d joint_angles(30, 30, 30);
-    cout << "Vector Form:" << endl << itou(kin(joint_angles)) << endl;
-    cout << "Matrix Form:" << endl << kin(joint_angles) << endl;
+    Vector3d joint_angles_deg(45, -30, 30);
+    Vector3d link_lengths_m(0.5, 0.5, 0);
+    Matrix3d wrelb = kin(joint_angles_deg, link_lengths_m);
+    cout << "Vector Form:" << endl << itou(wrelb) << endl;
+    cout << "Matrix Form:" << endl << wrelb << endl;
     return 0;
 }
