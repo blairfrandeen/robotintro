b3fe1a1a424d21faa8899dd1c7d43a0f43f02f1e
Blair Frandeen <blairfrandeen@gmail.com>
2024-04-05 12:35:56 -0700

    basic graphics functionality

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 #include <iostream>
 
 #include "SFML/Graphics/CircleShape.hpp"
+#include "SFML/Graphics/RectangleShape.hpp"
 #include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/Vertex.hpp"
+#include "SFML/System/Vector2.hpp"
 #include "robotlib.h"
 
 using Eigen::Matrix2d;
@@ -14,6 +17,9 @@ using Eigen::Vector2d;
 using Eigen::Vector3d;
 
 using namespace std;
+
+// multiply meters by this number to get into graphics.
+const double SFML_SCALE = 400.f;
 
 sf::VertexArray xyAxis(sf::RenderWindow& window) {
     // Create the X and Y axes
@@ -40,6 +46,36 @@ sf::VertexArray xyAxis(sf::RenderWindow& window) {
     xyAxis[3].color = sf::Color::Blue;
 
     return xyAxis;
+}
+
+sf::Vector2f _transform_point(sf::Vector2f point_m, sf::RenderWindow& window) {
+    sf::Vector2u win_size = window.getSize();
+    double x_offset = win_size.x / 2.f + point_m.x * SFML_SCALE;
+    double y_offset = win_size.y / 2.f - point_m.y * SFML_SCALE;
+
+    return sf::Vector2f(x_offset, y_offset);
+}
+
+/* Given a link origin and length, draw a rectangle representing the link
+   starting at the origin vertex, with a given length and angle measured from
+   the X axis.
+*/
+sf::RectangleShape link(sf::Vector2f origin, double len_m, double angle_deg) {
+    // Create a rectangle shape
+    sf::RectangleShape linkRect(
+        sf::Vector2f(len_m * SFML_SCALE, 10.0f));  // Width = len, Height = 10
+
+    // Set the origin of the rectangle to the center of the left edge
+    linkRect.setOrigin(0.0f, linkRect.getSize().y / 2.0f);
+
+    // Set the position of the rectangle
+    linkRect.setPosition(origin);
+
+    // Rotate the rectangle around its origin
+    linkRect.setRotation(-angle_deg);
+
+    // Return the rotated and positioned rectangle
+    return linkRect;
 }
 
 int main() {
@@ -72,12 +108,15 @@ int main() {
 
         window.clear();
 
-        sf::CircleShape shape(50.f);
-        shape.setFillColor(sf::Color(100, 250, 50));
         // draw things here:
         sf::VertexArray axs = xyAxis(window);
+        sf::RectangleShape lnk_1 =
+            link(_transform_point(sf::Vector2f(0, 0), window), 0.5, 30);
+        sf::RectangleShape lnk_2 =
+            link(_transform_point(sf::Vector2f(0.433, 0.25), window), 0.5, 60);
         window.draw(axs);
-        window.draw(shape);
+        window.draw(lnk_1);
+        window.draw(lnk_2);
 
         window.display();
     }
